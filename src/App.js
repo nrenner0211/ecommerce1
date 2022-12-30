dc70cfe186a82131d5e2362b20eb56c03625809c
nrenner <nrenner0211@gmail.com>
2022-12-30 15:57:12 -0500

    cart functionality added

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Navbar } from './components/Navbar';
 import { Shop } from './pages/shop/Shop';
 import { Cart } from './pages/cart/Cart';
+import { ShopContextProvider } from './context/ShopContext';
 
 function App() {
   return (
     <div className="App">
+      <ShopContextProvider>
       <Router>
         <Navbar />
         <Routes>
@@ -16,6 +18,7 @@ function App() {
           <Route path="/cart" element={<Cart/>}/>
         </Routes>
       </Router>
+      </ShopContextProvider>
     </div>
   );
 }
